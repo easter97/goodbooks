9730d076a93853b91bd09cd4f27a24b20c4cfa94
Amanda Easter <me@amandaeaster.com>
2024-08-12 13:43:45 -0500

    Goodbooks init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { ShelfComponent } from './components/shelf/shelf.component';
+import { BookComponent } from './components/book/book.component';
+import { RemoveParenthesisPipe } from './pipes/remove-parenthesis.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShelfComponent,
+    BookComponent,
+    RemoveParenthesisPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
